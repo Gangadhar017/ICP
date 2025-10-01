878d4445bcffc5cccb810a9bc25aa3a21d35fd5f
Gangadhar017 <156058881+Gangadhar017@users.noreply.github.com>
2025-10-01 11:14:30 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java

--- Assign4/LongestSubstringWithoutRepeatingCharacters.java
+++ Assign4/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,14 +5,16 @@ public class LongestSubstringWithoutRepeatingCharacters {
     public static int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         int left = 0, maxLength = 0;
-
+        // Sliding window approach lagayenge
+        // Jab tak right pointer aage badhta hai, tab tak hum character ko set mein dalte rahenge
+        // Agar character already set mein hai, toh hum left pointer ko aage badhate rahenge
+        // Jab tak duplicate character milta hai, tab tak left pointer ko aage badhate rahenge
+        // Har step par maxLength ko update karte rahenge
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
-
             if (map.containsKey(c)) {
                 left = Math.max(left, map.get(c) + 1);
             }
-
             map.put(c, right);
             maxLength = Math.max(maxLength, right - left + 1);
         }
