ef8ec5e4a0cea8d1b5a00fbd736a63d959dafe5a
Gangadhar017 <156058881+Gangadhar017@users.noreply.github.com>
2025-10-01 11:12:26 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java

--- Assign4/LongestSubstringWithoutRepeatingCharacters.java
+++ Assign4/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,27 +1,22 @@
-class ques {
-    public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        int maxLength = 0;
-        HashSet<Character> charSet = new HashSet<>();
-        //Sliding window approach lagayenge
-        // Hum left aur right pointers ka use karenge
-        // Jab tak right pointer aage badhta hai, tab tak hum character ko set mein dalte rahenge
-        // Agar character already set mein hai, toh hum left pointer ko aage badhate rahenge
+package Assignment4;
+import java.util.*;
+public class LongestSubstringWithoutRepeatingCharacters {
 
-        // Jab tak duplicate character milta hai, tab tak left pointer ko aage badha
-        // Jab duplicate character mil jaye, tab hum left pointer ko aage badhate rahenge
-        // Aur right pointer ko bhi aage badhate rahenge
-        
+    public static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLength = 0;
+
         for (int right = 0; right < s.length(); right++) {
-            while (charSet.contains(s.charAt(right))) {
-                charSet.remove(s.charAt(left));
-                left++;
+            char c = s.charAt(right);
+
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
             }
-            // Add the current character to the set
-            charSet.add(s.charAt(right));
+
+            map.put(c, right);
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
-        return maxLength;       
+        return maxLength;
     }
 }
