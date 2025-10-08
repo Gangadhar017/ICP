0d0ed2eaac965daef14489db1ac8ec563b3d5140
Gangadhar017 <156058881+Gangadhar017@users.noreply.github.com>
2025-10-08 09:11:34 +0530

    Update onlineStockSpan.java

--- Assign8/onlineStockSpan.java
+++ Assign8/onlineStockSpan.java
@@ -1,1 +1,17 @@
+class StockSpanner {
+    Stack<Pair<Integer,Integer>> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int cnt = 1;
+        while(!st.isEmpty() && st.peek().getKey() <= price){
+            cnt += st.pop().getValue();
+        }
+        st.push(new Pair(price, cnt));
+        return cnt;
+    }
+}
 
